8e2a7684b5bc2eaaa7690f873944c7a29bfd911c
yasmine By <benyasminenl@gmail.com>
2020-05-14 18:00:37 +0200

    Latest commit before trying to fix the date

---
+++ attempt2/app/src/main/java/com/example/gamebacklog/ui/addgame/AddGameActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.gamebacklog.ui.addgame
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class AddGameActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+}
